86d19e5990c51c84682cdd9007dcebf89c888774
Keith Dingle <keith.dingle@leadingagile.com>
2022-05-04 08:39:21 -0500

    setup

---
+++ src/main/java/com/kmd/StoreRestfulAPI/StoreRestfulApiApplication.java
@@ -0,0 +1,13 @@
+package com.kmd.StoreRestfulAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreRestfulApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreRestfulApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kmd/StoreRestfulAPI/StoreRestfulApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kmd.StoreRestfulAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreRestfulApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
