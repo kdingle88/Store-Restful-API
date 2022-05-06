63ec44ee054be31794df9fb674e39d1307625aad
Keith Dingle <keith.dingle@leadingagile.com>
2022-05-06 13:24:15 -0500

    add DirtiesContext annotation to clear database before each test

--- src/test/java/com/kmd/StoreRestfulAPI/ProductControllerIntegrationTest.java
+++ src/test/java/com/kmd/StoreRestfulAPI/ProductControllerIntegrationTest.java
@@ -1,15 +1,20 @@
 package com.kmd.StoreRestfulAPI;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.annotation.DirtiesContext;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 public class ProductControllerIntegrationTest {
 
     @LocalServerPort
